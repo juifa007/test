ae4a84bbeb5203c654345048399cea66314a031a
Ray-PC\Ray <juifa007@gmail.com>
2017-10-30 18:38:20 +0800

    第一個 php 語法

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <h1>Hello World !!! From PHP</h1>
     <H2>再次重頭來，要重複幾百次啊！！ From PHP</H2>
 <?php
-    echo "This is after Hypertext Preprocessor !!"
+    echo "This is after Hypertext Preprocessor !!!!!!!!!!"
 ?>
 </body>
 </html>
