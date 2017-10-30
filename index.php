77fc90b45b53c1204210a1b43966447f2200637b
Ray-PC\Ray <juifa007@gmail.com>
2017-10-30 18:36:22 +0800

    刪掉 html 改用 php 來做網頁

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Again!!Again!!!</title>
+</head>
+<body>
+    <h1>Hello World !!! From PHP</h1>
+    <H2>再次重頭來，要重複幾百次啊！！ From PHP</H2>
+<?php
+    echo "This is after Hypertext Preprocessor !!"
+?>
+</body>
+</html>
